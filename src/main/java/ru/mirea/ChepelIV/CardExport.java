b2a18ba0015f551aaef97d5a025a0e7c16199353
Dorikan <ci06122004@gmail.com>
2024-09-30 17:16:06 +0300

    serialization time удалено НАФИГ

--- src/main/java/ru/mirea/ChepelIV/CardExport.java
+++ src/main/java/ru/mirea/ChepelIV/CardExport.java
@@ -3,14 +3,12 @@ package ru.mirea.ChepelIV;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
-import java.util.Date;
 
 public class CardExport {
     public static void cardSerialization(Card target) {
         try {
-            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("C:\\Users\\User\\IdeaProjects\\Pkmn\\src\\main\\resources\\" + target.getName() + ".crd"));
-            Date now = new Date(System.currentTimeMillis());
-            out.writeObject(now);
+            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("src\\main\\resources\\" + target.getName() + ".crd"));
+
             out.writeObject(target);
             out.flush();
             out.close();
--- src/main/java/ru/mirea/ChepelIV/PkmnApplication.java
+++ src/main/java/ru/mirea/ChepelIV/PkmnApplication.java
@@ -1,8 +1,8 @@
 package ru.mirea.ChepelIV;
 
 public class PkmnApplication {
-    public static void main(String[] args) {
-        Card myCard = CardImport.importCardFromTxt("C:\\Users\\User\\IdeaProjects\\Pkmn\\src\\main\\resources\\my_card.txt");
+    public static void main(String[] args) throws ClassNotFoundException {
+        Card myCard = CardImport.cardImportByte("src\\main\\resources\\Gengar Gigantamax.crd");
         System.out.println(myCard.toString());
         CardExport.cardSerialization(myCard);
     }
