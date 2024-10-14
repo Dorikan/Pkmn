c767043cd2ba9e69af8e0cc29e1e583b2673a3f0
Dorikan <ci06122004@gmail.com>
2024-10-14 10:32:18 +0300

    refactor

---
+++ src/main/java/ru/mirea/pkmn/EnergyType.java
@@ -0,0 +1,15 @@
+package ru.mirea.pkmn.ChepelIV;
+
+public enum EnergyType {
+    FIRE,
+    GRASS,
+    WATER,
+    LIGHTNING,
+    PHYSIC,
+    FIGHTING,
+    DARKNESS,
+    METAL,
+    FAIRY,
+    DRAGON,
+    COLORLESS
+}
---
+++ src/main/java/ru/mirea/pkmn/PokemonStage.java
@@ -0,0 +1,9 @@
+package ru.mirea.pkmn.ChepelIV;
+
+public enum PokemonStage {
+    BASIC,
+    STAGE1,
+    STAGE2,
+    VSTAR,
+    VMAX
+}
